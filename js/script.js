7951c4639ed242998de17f461504d6bb6a77de8d
Johnny Ji <johnny.ji@live.ca>
2015-01-13 20:11:41 -0800

    fixed background container sizing issue, added a working sidebar that animates out

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 $(window).load(function() {
+	// loads fade in effects
    $('.name').fadeIn(1000);
    $('.links').delay(700).fadeIn(600);
    $('.footer').delay(1600).fadeIn(500);
+
+   bacon()
+
 });
+
+function bacon() {
+	$('.open_icon').click(function() {
+
+		$(this).fadeOut(300);
+		$('.close_icon').fadeIn(300);
+
+		$('.container_frame').animate({
+			left: '-200px'
+		})
+
+		$('.sidebar').animate({
+			right: '0px'
+		})
+	}); 
+
+	$('.close_icon').click(function() {
+		$(this).fadeOut(300);
+		$('.open_icon').fadeIn(300);
+
+		$('.container_frame').animate({
+			left: '0px'
+		})
+
+		$('.sidebar').animate({
+			right: '-200px'
+		})
+	});
+}
